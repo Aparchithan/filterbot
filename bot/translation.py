d202b7287560d855b0fd2762fe9b5f8eec5d59ad
Aparchithan <88177437+Aparchithan@users.noreply.github.com>
2021-08-01 22:53:22 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,10 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Iam for kithabukalude kalavara you can't add me in your group 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@kithabukaludekalavara
 </i>"""    
     
     HELP_TEXT = """
@@ -43,7 +43,7 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@kithabukaludekalavara
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
@@ -54,5 +54,5 @@ For More Details Click Help Button Below..
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Aparchithan/filterbot">Click Me</a></i>
 """
